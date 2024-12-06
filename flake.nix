d87cc55aa7d7585cf35f21c56ce29423f6f36c33
Ivan Mincik <ivan.mincik@gmail.com>
2024-12-06 11:59:21 +0100

    flake: allow unfree nixpkgs

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       # Flake system
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; config.allowUnfree = true; });
 
       # Version
       stateVersion = "24.11";
